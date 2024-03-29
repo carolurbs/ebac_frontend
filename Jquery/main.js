b041e243f7e5a01f38d3e44f6c0201f37fe6d8da
Caroline Urbainski <caroline.urbainski@hotmail.com>
2024-03-28 22:00:15 -0300

    Lista de Tarefas

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     console.log($('form button'))
+   
 })
 
 $('form').on('submit',function(e){
-    e.preventDefaut();
+    e.preventDefault();
+    const newTask = $('#task').val();
+    const newItem = $('<li></li>').text(newTask);
+   $('ul').append(newItem);
+   newItem.on('click', function(){
+   
+       $(this).toggleClass('completed'); 
+   
+   
+   })
 })
