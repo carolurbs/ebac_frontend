e953796bdb627249d295d94197a5eb978966d298
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-08-05 17:27:38 -0300

    aJAX

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(){
-const nameProfile = document.querySelector('profile-name');
-const userProfile =document.querySelector('profile-username');
-const avatarProfile =document.querySelector('profile-avatar');
-const bioProfile =document.querySelector('profile-bio');
-const locationProfile =document.querySelector('profile-location');
-const companyProfile =document.querySelector('profile-company');
-const reposProfile =document.querySelector('profile-repos');
-const followersProfile =document.querySelector('profile-followers');
-const followingProfile =document.querySelector('profile-following');
-const linkToProfile =document.querySelector('profile-link');
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const nameProfile = document.querySelector('.profile-name');
+    const userProfile = document.querySelector('.profile-username');
+    const avatarProfile = document.querySelector('.profile-avatar');
+    const bioProfile = document.querySelector('.profile-bio');
+    const locationProfile = document.querySelector('.profile-location');
+    const reposProfile = document.querySelector('.profile-repos');
+    const followersProfile = document.querySelector('.profile-followers');
+    const followingProfile = document.querySelector('.profile-following');
+    const linkToProfile = document.querySelector('.profile-link');
 
-fetch('https://api.github.com/users/carolurbs')
-.then(function(res){
-    return res.json();
-})
-.then(function(json){
-    nameProfile.innerText=json.name;
-    userProfile.innerText=json.login;
-    avatarProfile.src=json.avatar_url;
-    reposProfile.innerText=json.public_repos;
-    followersProfile.innerText=json.followers;
-    followingProfile.innerText=json.following;
-    bioProfile.innerText=json.bio;
-    locationProfile.innerText=json.location;
-    companyProfile.innerText=json.company;
-    linkToProfile.href=json.html_url;
-})
+    fetch('https://api.github.com/users/carolurbs')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            nameProfile.innerText = json.name;
+            userProfile.innerText = json.login;
+            avatarProfile.src = json.avatar_url;
+            reposProfile.innerText = json.public_repos;
+            followersProfile.innerText = json.followers;
+            followingProfile.innerText = json.following;
+            bioProfile.innerText = json.bio;
+            locationProfile.innerText = json.location;
+            linkToProfile.href = json.html_url;
+        })
+        .catch(function(error) {
+            alert('Ocorreu um erro:', error);
+        });
+});
