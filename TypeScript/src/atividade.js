a0b4b7f6d795a387d146248020cb498feb63583a
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-09-26 13:05:31 -0300

    atividade

---
+++ TypeScript/src/atividade.js
@@ -0,0 +1,13 @@
+"use strict";
+function calculaArea(base, altura) {
+    base = 20;
+    altura = 35;
+    return base * altura;
+}
+function dizOi(frase, nome) {
+    frase = "Oi";
+    nome = "Carol";
+    return frase + nome;
+}
+console.log(calculaArea);
+console.log(dizOi);
--- TypeScript/src/atividade.ts
+++ TypeScript/src/atividade.ts
@@ -1,1 +1,15 @@
-let nome= 'Carol';
+
+function calculaArea(base: number, altura:number):number{
+    base= 20;
+    altura=35;
+    return base*altura;
+}
+
+function dizOi(frase:string, nome:string):string{
+    frase= "Oi"
+    nome= "Carol"
+    return frase + nome;
+}
+
+console.log(calculaArea);
+console.log(dizOi);
